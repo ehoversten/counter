ca28d446d9359ebf78cc7e340b690fd54895d702
ehoversten <ehoversten@gmail.com>
2018-05-04 16:44:21 -0400

    what the huh??

--- server.py
+++ server.py
@@ -8,14 +8,30 @@
 #
 # ------------------------------------------------------------------------------/
 
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = '42'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'count' not in session:
+        session['count'] = 0
+    # session['count'] = str(int(session['count']) + 1)
+    session['count'] += 1
+
+    return render_template("index.html", count=session['count'])
+
+@app.route('/destroy', methods=['POST'])
+def destroy():
+    session.clear()
+    return redirect('/')
+
+@app.route('/add', methods=['POST'])
+def add():
+    session['count'] += 1
+    return redirect('/')
+
 
 if __name__=="__main__":
-
     app.run(debug=True)
